fdad4f2db486522f62158170181fae15621e3bd5
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-13 20:47:33 +0100

    implemented onInteraction start and onInteraction update.

--- controller/controller.ts
+++ controller/controller.ts
@@ -1,32 +1,14 @@
 import {DnD} from "../models/interaction";
+import {Drawing, Line, Rectangle, Shape} from "../models/model";
+import {drawPaint} from "../view/view";
 
 export class controller {
-    private _editingMode = {rect: 0, line: 1};
-    private _currEditingMode: number | SVGLineElement;
-    private _currLineWidth: number;
-    private _currColour: string;
-    private _currentShape: number;
     private dragNdrop: DnD;
+    private drawing: Drawing;
+    private ctx: CanvasRenderingContext2D;
+    private canvas: HTMLElement;
 
-    pencil(ctx, drawing, canvas) {
-        this._currEditingMode = this._editingMode.line;
-        this._currLineWidth = 5;
-        this._currColour = '#000000';
-        this._currentShape = 0;
-        // Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
-        this.dragNdrop = new DnD(canvas, this);
-    };
-
-    onInteractionStart(dnd:DnD){
-    };
-
-    onInteractionUpdate(dnd:DnD){
-
-    };
-
-    onInteractionEnd(dnd:DnD){
-
-    };
+    private _editingMode = {rect: 0, line: 1};
 
     get editingMode(): { rect: number; line: number } {
         return this._editingMode;
@@ -36,6 +18,8 @@ export class controller {
         this._editingMode = value;
     }
 
+    private _currEditingMode: number | SVGLineElement;
+
     get currEditingMode(): number | SVGLineElement {
         return this._currEditingMode;
     }
@@ -43,6 +27,8 @@ export class controller {
     set currEditingMode(value: number | SVGLineElement) {
         this._currEditingMode = value;
     }
+
+    private _currLineWidth: number;
 
     get currLineWidth(): number {
         return this._currLineWidth;
@@ -52,6 +38,8 @@ export class controller {
         this._currLineWidth = value;
     }
 
+    private _currColour: string;
+
     get currColour(): string {
         return this._currColour;
     }
@@ -60,11 +48,73 @@ export class controller {
         this._currColour = value;
     }
 
-    get currentShape(): number {
+    private _currentShape: Shape;
+
+    get currentShape(): Shape {
         return this._currentShape;
     }
 
-    set currentShape(value: number) {
-        this._currentShape = value;
+    set currentShape(shape: Shape) {
+        this._currentShape = shape;
     }
+
+    pencil(ctx, drawing, canvas) {
+        this._currEditingMode = this._editingMode.line;
+        this._currLineWidth = 5;
+        this._currColour = '#ff9c7a';
+        this._currentShape = null;
+        this.ctx = ctx;
+        this.drawing = drawing;
+        this.canvas = canvas;
+        // Liez ici les widgets à la classe pour modifier les attributs présents ci-dessus.
+        this.dragNdrop = new DnD(canvas, this);
+    };
+
+    onInteractionStart() {
+        this.currLineWidth = +(<HTMLInputElement>document.getElementById('spinnerWidth')).value;
+        this.currColour = (<HTMLInputElement>document.getElementById('colour')).value;
+        let recButton = (<HTMLInputElement>document.getElementById('butRect')).checked;
+        let lineButton = (<HTMLInputElement>document.getElementById('butLine')).checked;
+        if (recButton) {
+            this.currEditingMode = 0;
+        } else if (lineButton) {
+            this.currEditingMode = 1;
+        }
+
+        //switch sur une ligne ou un rectangle et affectation à la forme courante
+        console.log(this.currEditingMode);
+        switch (this.currEditingMode) {
+
+            case 0:
+                this.currentShape = new Rectangle(this.currColour, this.dragNdrop.xInit, this.dragNdrop.yInit
+                    , 0, 0, this.currLineWidth);
+                break;
+            case 1:
+                this.currentShape = new Line(this.currColour, this.dragNdrop.xInit, this.dragNdrop.yInit
+                    , 0, 0, this.currLineWidth);
+                break;
+        }
+        this.drawing.addShape(this.currentShape);
+        drawPaint(this.ctx, this.drawing, this.canvas);
+        //this.currentShape.paint(ctx, canvas);
+    };
+
+    onInteractionUpdate() {
+        switch (this.currEditingMode) {
+            case 0:
+                this.currentShape = new Rectangle(this.currColour, this.dragNdrop.xInit, this.dragNdrop.yInit
+                    , 0, 0, this.currLineWidth);
+                break;
+            case 1:
+                this.currentShape = new Line(this.currColour, this.dragNdrop.xInit, this.dragNdrop.yInit
+                    , 0, 0, this.currLineWidth);
+                break;
+        }
+        this.drawing.addShape(this.currentShape);
+        drawPaint(this.ctx, this.drawing, this.canvas);
+    };
+
+    onInteractionEnd() {
+
+    };
 }
