7b0745f6be01032b703a4f30b1c635e3df01584f
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-13 17:49:28 +0100

    update model classes.

--- models/model.ts
+++ models/model.ts
@@ -1,65 +1,156 @@
+export class Drawing{
+
+    private _shapes : Array<Shape>;
+
+    constructor() {
+        this._shapes = [];
+    }
+
+    addShape(shape : Shape) : void {
+        this._shapes.push(shape);
+    }
+
+    removeShape(index:number) : void {
+        if (index > -1) {
+            this._shapes.splice(index, 1);
+        }
+    }
+
+    get shapes(): Array<Shape> {
+        return this._shapes;
+    }
+
+}
+
 export class Shape {
-    private _xInit:number;
-    private _yInit:number;
-    private _xFinal: number;
-    private _yFinal: number;
-    private _lineWidth:string|number;
-    private _color:string;
+    private _colour: string;
+    private _thickness: number;
 
-    constructor(xInit:number,yInit:number, xFinal:number, yFinal:number, lineWidth: string|number, color:string ){
-        this.xInit = xInit;
-        this.yInit = yInit;
-        this.xFinal = xFinal;
-        this.yFinal = yFinal;
-        this.lineWidth = lineWidth;
-        this.color = color;
+    constructor(colour: string, thickness: number) {
+        this._colour = colour;
+        this._thickness = thickness;
     }
 
-    get color(): string {
-        return this._color;
+    get colour(): string {
+        return this._colour;
     }
 
-    set color(value: string) {
-        this._color = value;
+    set colour(value: string) {
+        this._colour = value;
     }
 
-    get lineWidth(): string | number {
-        return this._lineWidth;
+    get thickness(): number {
+        return this._thickness;
     }
 
-    set lineWidth(value: string | number) {
-        this._lineWidth = value;
-    }
-    get yFinal(): number {
-        return this._yFinal;
-    }
-
-    set yFinal(value: number) {
-        this._yFinal = value;
-    }
-    get xFinal(): number {
-        return this._xFinal;
-    }
-
-    set xFinal(value: number) {
-        this._xFinal = value;
-    }
-    get yInit(): number {
-        return this._yInit;
-    }
-
-    set yInit(value: number) {
-        this._yInit = value;
-    }
-    get xInit(): number {
-        return this._xInit;
-    }
-
-    set xInit(value: number) {
-        this._xInit = value;
+    set thickness(value: number) {
+        this._thickness = value;
     }
 }
 
 
-export class Rectangle extends Shape{}
-export class Line extends Shape{}
+export class Rectangle extends Shape {
+
+    private _xCoordinate: number;
+    private _yCoordinate: number;
+    private _height: number;
+    private _width: number;
+
+    constructor(colour: string, thickness: number, xCoordinate: number, yCoordinate: number, height: number, widht: number) {
+        super(colour, thickness);
+        this.xCoordinate = xCoordinate;
+        this.yCoordinate = yCoordinate;
+        this.height = height;
+        this.width = widht;
+    }
+
+    get xCoordinate(): number {
+        return this._xCoordinate;
+    }
+
+    set xCoordinate(value: number) {
+        this._xCoordinate = value;
+    }
+
+    get yCoordinate(): number {
+        return this._yCoordinate;
+    }
+
+    set yCoordinate(value: number) {
+        this._yCoordinate = value;
+    }
+
+    get height(): number {
+        return this._height;
+    }
+
+    set height(value: number) {
+        this._height = value;
+    }
+
+    get width(): number {
+        return this._width;
+    }
+
+    set width(value: number) {
+        this._width = value;
+    }
+
+    xCoordinateFinal () : number {
+        return this.xCoordinate + this.width;
+    };
+
+    yCoordinateFinal() : number {
+        return this.yCoordinate + this.height;
+    };
+}
+
+export class Line extends Shape {
+
+    private _xCoordinateInit: number;
+    private _yCoordinateInit: number;
+    private _xCoordinateFinal: number;
+    private _yCoordinateFinal: number;
+
+    constructor(colour: string, thickness: number, xCoordinateInit: number, yCoordinateInit: number, xCoordinateFinal: number, yCoordinateFinal: number) {
+        super(colour, thickness);
+        this.xCoordinateInit = xCoordinateInit;
+        this.yCoordinateInit = yCoordinateInit;
+        this.xCoordinateFinal = xCoordinateFinal;
+        this.yCoordinateFinal = yCoordinateFinal;
+    }
+
+    get xCoordinateInit(): number {
+        return this._xCoordinateInit;
+    }
+
+    set xCoordinateInit(value: number) {
+        this._xCoordinateInit = value;
+    }
+
+    get yCoordinateInit(): number {
+        return this._yCoordinateInit;
+    }
+
+    set yCoordinateInit(value: number) {
+        this._yCoordinateInit = value;
+    }
+
+    get xCoordinateFinal(): number {
+        return this._xCoordinateFinal;
+    }
+
+    set xCoordinateFinal(value: number) {
+        this._xCoordinateFinal = value;
+    }
+
+    get yCoordinateFinal(): number {
+        return this._yCoordinateFinal;
+    }
+
+    set yCoordinateFinal(value: number) {
+        this._yCoordinateFinal = value;
+    }
+
+
+}
