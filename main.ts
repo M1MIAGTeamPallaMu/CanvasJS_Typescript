dc3af6d3d3df560a6921ab8dd2d027504be0f283
Abraham Mubanzo <ab.learn.code@gmail.com>
2018-03-13 18:18:31 +0100

    added draw test. works but i notice no colour change

--- main.ts
+++ main.ts
@@ -1,17 +1,21 @@
 import {DnD} from "./models/interaction";
+import {Drawing, Line, Rectangle} from "./models/model";
+import {linePaint, rectPaint} from "./view/view";
 
-class Main{
+class Main {
     private _canvas;
-    private _ctx : any;
-    private _dnd : DnD;
-    constructor(){
+    private _ctx: any;
+    private _dnd: DnD;
+
+    constructor() {
         this.canvas = document.getElementById('myCanvas');
         this.canvas.width = 900;
         this.canvas.height = 600;
         this.ctx = this._canvas.getContext('2d');
     }
+
     // Code temporaire pour tester le DnD
-    DnDTest(){
+    DnDTest() {
         this.dnd = new DnD(this.canvas);
         this.canvas.onmousedown = (event: MouseEvent) => {
             this.dnd.select(event);
@@ -25,12 +29,40 @@ class Main{
         this.ctx.fillStyle = '#F0F0F0'; // set canvas' background color
         this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);  // now fill the canvas
     }
+
+    // Code temporaire pour tester l'affichage de la vue
+    DrawTest(){
+        let drawing = new Drawing();
+        let rec = new Rectangle('#ccc61c', 20, 50, 100, 5, 60);
+        let rec1 = new Rectangle('#37e1ee', 120, 50, 100, 105,210);
+        let rec2 = new Rectangle('#cc3b3b', 220, 450, 300, 55, 70);
+
+        rectPaint(this.ctx, rec);
+        rectPaint(this.ctx, rec1);
+        rectPaint(this.ctx, rec2);
+
+        drawing.addShape(rec);
+        drawing.addShape(rec1);
+        drawing.addShape(rec2);
+
+        let line1 = new Line('#cc8575', 40, 50, 100, 225, 710);
+        let line2 = new Line('#ccb55b', 30, 50, 200, 250, 410);
+        let line3 = new Line('#4164cc', 50, 50, 300, 600, 190);
+
+        linePaint(this.ctx, line1);
+        linePaint(this.ctx, line2);
+        linePaint(this.ctx, line3);
+
+    }
+
     get canvas(): any {
         return this._canvas;
     }
+
     set canvas(value) {
         this._canvas = value;
     }
+
     get ctx(): any {
         return this._ctx;
     }
@@ -47,5 +79,7 @@ class Main{
         this._dnd = value;
     }
 }
+
 let test = new Main();
-test.DnDTest();
+//test.DnDTest();
+test.DrawTest();
